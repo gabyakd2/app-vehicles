1f29886e42663517692a9fd641fd62277be695d5
gabyakd2 <gabrielsal00@hotmail.com>
2023-07-26 18:11:11 -0300

    interface vehicle ready and new dependencis

--- app/Home/components/CardVehicle/CardVehicle.tsx
+++ app/Home/components/CardVehicle/CardVehicle.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import {CardContent, Typography, CardActionArea } from '@mui/material';
+import { Vehicle } from './model/vehicle.model';
 
-function CardVehicle () {
+function CardVehicle ({name, year, price, photo}: Vehicle) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
---
+++ app/Home/components/CardVehicle/model/vehicle.model.ts
@@ -0,0 +1,9 @@
+export interface Vehicle {
+  id: number;
+  name: string;
+  segment: string;
+  year: number;
+  price: number;
+  thumbnail: string;
+  photo: string;
+}
