ab4e01f36d723a2c6f3b2c94aa00861c4add4953
gabyakd2 <gabrielsal00@hotmail.com>
2023-07-25 23:57:24 -0300

    test

--- app/Home/components/CardVehicle/CardVehicle.tsx
+++ app/Home/components/CardVehicle/CardVehicle.tsx
@@ -9,12 +9,6 @@ function CardVehicle () {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             Lizard
