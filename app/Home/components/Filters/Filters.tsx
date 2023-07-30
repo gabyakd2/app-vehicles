64e8b50cc32540393352c1550c338394f4d027be
gabyakd2 <gabrielsal00@hotmail.com>
2023-07-30 16:17:26 -0300

    creation of filters in progress

--- app/Home/components/Filters/Filters.tsx
+++ app/Home/components/Filters/Filters.tsx
@@ -2,13 +2,26 @@ import { Container } from "@mui/material";
 import React from "react";
 import SelectOrder from "./components/SelectOrder";
 import { Button, Stack } from "@mui/material";
+import { FiltersState } from "../../Home";
 
-function Filters() {
+interface Props {
+    setFilters: React.Dispatch<React.SetStateAction<FiltersState>>;
+}
+
+function Filters({setFilters}: Props) {
+
+  const handleChangeSegment = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setFilters((prev) => ({
+      ...prev,
+      segment: 'todos',
+    }));
+  }
+
   return (
     <Container className="flex justify-between my-10">
         <p className="mt-3">Filtrar por</p>
       <Stack spacing={2} direction="row">
-        <Button className="normal-case text-base" variant="text">Todos</Button>
+        <Button className="normal-case text-base" variant="text" onClick={handleChangeSegment} >Todos</Button>
         <Button className="normal-case text-base" variant="text">Autos</Button>
         <Button className="normal-case text-base" variant="text">Pickups y Comerciales</Button>
         <Button className="normal-case text-base" variant="text">SUVs y Crossovers</Button>
--- app/Home/components/Filters/components/SelectOrder.tsx
+++ app/Home/components/Filters/components/SelectOrder.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import {InputLabel, FormControl, NativeSelect} from '@mui/material/';
 
