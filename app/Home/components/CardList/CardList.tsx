003bf6b6db74c685afe0ac9d6e63c813a063aa28
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-01 20:08:59 -0300

    filters ready perfect

--- app/Home/components/CardList/CardList.tsx
+++ app/Home/components/CardList/CardList.tsx
@@ -11,17 +11,38 @@ interface Props {
 function CardList({data, filters}: Props) {
   const aux = data
 // console.log(aux)
-  const filterVehicles = aux?.filter((vehicle) => (
-    (filters.category === "all" || vehicle.segment === filters.category) &&
-    (filters.orderBy === 0 || vehicle.price >= filters.orderBy)
-  ));
+const filteredByCategory = filters.category === 'all'
+? aux
+: aux.filter((vehicle) => vehicle.segment === filters.category);
 
-// console.log(filterVehicles)
+// Ordenar los resultados por precio y año de lanzamiento
+const filteredAndSorted = filteredByCategory.sort((vehicleA, vehicleB) => {
+if (filters.orderBy === 0) {
+  // Sin ordenamiento
+  return 0;
+} else if (filters.orderBy === 1) {
+  // Ordenar por precio de menor a mayor
+  return vehicleA.price - vehicleB.price;
+} else if (filters.orderBy === 2) {
+  // Ordenar por precio de mayor a menor
+  return vehicleB.price - vehicleA.price;
+} else if (filters.orderBy === 3) {
+  // Ordenar por año de lanzamiento de menor a mayor
+  return vehicleA.year - vehicleB.year;
+} else if (filters.orderBy === 4) {
+  // Ordenar por año de lanzamiento de mayor a menor
+  return vehicleB.year - vehicleA.year;
+}
+// En caso de que filters.orderBy no coincida con ninguno de los valores anteriores
+return 0;
+});
+
+// console.log(filteredAndSorted)
   return (
     <div className="flex justify-center my-10">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-16">
-        {filterVehicles?.length &&
-          filterVehicles.map((vehicle: Vehicle) => (
+        {filteredAndSorted?.length &&
+          filteredAndSorted.map((vehicle: Vehicle) => (
             <CardVehicle
               id={vehicle.id}
               key={vehicle.id}
