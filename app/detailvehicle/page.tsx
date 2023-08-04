7cfb132c6c421c46eae5aa15576e602452283cb6
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-03 21:20:26 -0300

    new route detail

---
+++ app/detailvehicle/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+
+function DetailVehicle() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default DetailVehicle
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Home from "./Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import { Button, Grid, Stack } from "@mui/material";
 
 export default function App() {
   return (
