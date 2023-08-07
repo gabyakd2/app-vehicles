e547a17f4d7bdc4d1793c71157e926db1b5f5aff
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-07 01:48:52 -0300

    info by id ready

--- app/[id]/DetailVehicle/DetailVehicle.tsx
+++ app/[id]/DetailVehicle/DetailVehicle.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { useQuery } from "react-query";
 import { getDetailCart } from "./services/getDetailCart";
 import { CartDetail } from "./models/cartDetail";
+import TitleDetail from "./components/TitleDetail/TitleDetail";
+import { usePathname } from 'next/navigation'
 
 function DetailVehicle() {
-  // const { data, status } = useQuery<CartDetail, Error>("detailCart", () =>
-  //   getDetailCart(id)
-  // );
+  const pathName = usePathname();
+  const idPath = pathName.split("/")[1];
+  const id = parseInt(idPath)
+  const { data, status } = useQuery<CartDetail, Error>("detailCart", () =>
+    getDetailCart(id)
+  );
 
-  return <div></div>;
+  return (
+  <div>
+    <TitleDetail />
+  </div>
+  );
 }
 
 export default DetailVehicle;
