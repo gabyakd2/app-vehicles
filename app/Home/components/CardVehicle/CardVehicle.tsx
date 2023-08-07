a5ce49a402eebeead94ca5588105eef0e88f7f4c
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-07 11:28:09 -0300

    size card fix

--- app/Home/components/CardVehicle/CardVehicle.tsx
+++ app/Home/components/CardVehicle/CardVehicle.tsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import {CardContent, Typography, CardActionArea, CardMedia, Divider } from '@mui/material';
+import { CardContent, Typography, CardActionArea, CardMedia, Divider, Grid } from '@mui/material';
 import { Vehicle } from '../../models/vehicle.model';
 
-function CardVehicle ({name, year, price, photo}: Vehicle) {
+function CardVehicle({ name, year, price, photo }: Vehicle) {
   return (
-    <Card sx={{ maxWidth: 245 }}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" className='flex justify-center'>
-            {name}
-          </Typography>
-          <div className='flex justify-around my-4'>
-            <Typography variant="body2" color="text.secondary">
-              {year}
+    <Grid item xs={12} sm={6} md={4} lg={3}> {/* Ajusta los valores de xs, sm, md, lg según tus necesidades */}
+      <Card sx={{ maxWidth: 245, height: '100%' }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" className='flex justify-center'>
+              {name}
             </Typography>
-            <Divider orientation="vertical" flexItem />
-            <Typography variant="body2" color="text.secondary">
-              $ {price}
-            </Typography>
-          </div>
-        </CardContent>
-        <CardMedia
-          component="img"
-          image={photo}
-          alt={name}
-          className="object-cover"
-        />
-      </CardActionArea>
-    </Card>
+            <div className='flex justify-around my-4'>
+              <Typography variant="body2" color="text.secondary">
+                {year}
+              </Typography>
+              <Divider orientation="vertical" flexItem />
+              <Typography variant="body2" color="text.secondary">
+                $ {price}
+              </Typography>
+            </div>
+          </CardContent>
+          <CardMedia
+            component="img"
+            image={photo}
+            alt={name}
+            style={{ height: '110px' }}
+            className="object-cover"
+          />
+        </CardActionArea>
+      </Card>
+    </Grid>
   );
 }
 
-export default CardVehicle
+export default CardVehicle;
