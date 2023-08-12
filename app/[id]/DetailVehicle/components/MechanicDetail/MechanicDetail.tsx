4b78b546c22257631b61dae8774f869f86ef1394
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-12 02:39:12 -0300

    mechanic detail responsive

--- app/[id]/DetailVehicle/components/MechanicDetail/MechanicDetail.tsx
+++ app/[id]/DetailVehicle/components/MechanicDetail/MechanicDetail.tsx
@@ -10,29 +10,31 @@ function MechanicDetail({ data }: Props) {
   // console.log(data)
   return (
     <div>
-      <div className="flex justify-around items-center mb-2">
-        <div>
+      <div className="flex justify-evenly items-center my-20 flex-col md:flex-row">
+        <div className="md:ml-20">
           <p className="text-xl font-semibold">{data.model_features[0].name}</p>
           <p>{data.model_features[0].description}</p>
         </div>
         <Image
           src={data.model_features[0].image}
           alt="Image mechanic"
-          width={270}
+          width={370}
           height={100}
+          className="rounded-xl"
         />
       </div>
-      <div className="flex justify-around items-center mb-2">
-        <div>
-          <p className="text-xl font-semibold">{data.model_features[1].name}</p>
-          <p>{data.model_features[1].description}</p>
-        </div>
+      <div className="flex justify-evenly items-center mb-2 flex-col-reverse md:flex-row">
         <Image
           src={data.model_features[1].image}
           alt="Image mechanic"
-          width={270}
+          width={370}
           height={100}
+          className="rounded-xl"
         />
+        <div className="md:mr-20">
+          <p className="text-xl font-semibold">{data.model_features[1].name}</p>
+          <p>{data.model_features[1].description}</p>
+        </div>
       </div>
     </div>
   );
