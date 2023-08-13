73086001ff81de3691008b0717e0a5ae0b067346
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-13 15:06:44 -0300

    structure of filters resonsive

--- app/Home/components/Filters/Filters.tsx
+++ app/Home/components/Filters/Filters.tsx
@@ -1,10 +1,16 @@
+import React, { useState, useEffect } from "react";
 import { Container } from "@mui/material";
-import React from "react";
 import SelectOrder from "./components/SelectOrder";
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, FormControl, InputLabel, NativeSelect } from "@mui/material";
 import { SetFilterProp } from "./model/setFilterProp";
 
 function Filters({ setFilters }: SetFilterProp) {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768)
+  };
+
   const handleChangeCategory = (category: string) => {
     setFilters((prev) => ({
       ...prev,
@@ -12,9 +18,30 @@ function Filters({ setFilters }: SetFilterProp) {
     }));
   };
 
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Container className="flex justify-between my-10">
-      <p className="mt-3">Filtrar por</p>
+      {/* <p className="mt-3">Filtrar por</p> */}
+      {isMobile ? (
+        <FormControl>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Ordenar por
+        </InputLabel>
+        <NativeSelect>
+          <option>Todos</option>
+          <option>Autos</option>
+          <option>Pickups y Comerciales</option>
+          <option>SUVs y Crossovers</option>
+        </NativeSelect>
+        </FormControl>
+      ):
       <Stack spacing={2} direction="row">
         <Button
           className="normal-case text-base"
@@ -45,7 +72,8 @@ function Filters({ setFilters }: SetFilterProp) {
           SUVs y Crossovers
         </Button>
       </Stack>
-      <SelectOrder setFilters={setFilters}/>
+    }
+      <SelectOrder setFilters={setFilters} />
     </Container>
   );
 }
